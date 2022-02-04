0e052b81d4630d6eb879cb24f059b71d951ef788
Satoshi Matsushima <>
2022-02-04 23:46:09 +0900

    51. Loading Spinner

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,5 +1,6 @@
 import { data } from 'autoprefixer'
 import React, { useEffect, useState } from 'react'
+import Spinner from '../layout/Spinner'
 
 function UserResults() {
   const [users, setUsers] = useState([])
@@ -31,7 +32,7 @@ function UserResults() {
       </div>
     )
   } else {
-    return <h3>Loading...</h3>
+    return <Spinner />
   }
 }
 
