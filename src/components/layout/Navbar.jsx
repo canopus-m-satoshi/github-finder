d25d8e06f175447f4b4b49163d3cfb7e9a5fd15b
Satoshi Matsushima <>
2022-02-04 20:32:26 +0900

    46. Navbar Component

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -5,15 +5,31 @@ import PropTypes from 'prop-types'
 
 function Navbar({ title }) {
   return (
-    <nav className="navbar mb-2 shadow-lg bg-neutral text-neutral-content rounded-box">
-      <div className="container mx-auto">Navbar</div>
+    <nav className="navbar mb-2 shadow-lg bg-neutral text-neutral-content">
+      <div className="container mx-auto">
+        <div className="flex-none px-2 mx-2">
+          <FaGithub className="inline pr-2 text-3xl" />
+          <Link to="/" className="text-lg font-bold align-middle">
+            {title}
+          </Link>
+        </div>
 
-      <h1 classNmae="text-3xl font-bold underline">Hello world!</h1>
+        <div className="flex-1 px-2 mx-2">
+          <div className="fkex justify-end">
+            <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
+              Home
+            </Link>
+            <Link to="/about" className="btn btn-ghost btn-sm rounded-btn">
+              About
+            </Link>
+          </div>
+        </div>
+      </div>
     </nav>
   )
 }
 
-Navbar.defailtProps = {
+Navbar.defaultProps = {
   title: 'Github Finder',
 }
 
